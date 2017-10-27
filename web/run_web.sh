8488e66ce9fd4c435226a0593f73b3840b445e4d
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2017-10-27 18:11:45 -0200

    - Pequenas correções Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- web/run_web.sh
+++ web/run_web.sh
@@ -5,6 +5,6 @@ su -m estatistica -c "python manage.py makemigrations"
 # migrate db, so we have the latest db schema
 su -m estatistica -c "python manage.py migrate"
 # su -m estatistica -c "python manage.py createsu"
-# su -m root -c "python manage.py collectstatic --no-input"
+su -m root -c "python manage.py collectstatic --no-input"
 # start development server on public ip interface, on port 8000
 su -m root -c "/usr/local/bin/gunicorn trabalho_me_i.wsgi:application -w 2 -b :8000"
